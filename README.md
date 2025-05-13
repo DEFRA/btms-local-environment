cfc72f24e8a50a922d6500ef21344c851a311678
Tim Peel <tim.peel@equalexperts.com>
2025-05-13 11:38:47 +0100

    Add initial local environment

---
+++ README.md
@@ -0,0 +1,22 @@
+# BTMS local environment
+
+Requirements to run:
+
+- Docker
+
+Starting the local environment.
+
+```sh
+docker compose up
+```
+
+Services currently configured:
+
+- https://github.com/DEFRA/trade-imports-data-api
+- https://github.com/DEFRA/trade-imports-processor
+- https://github.com/DEFRA/trade-imports-decision-deriver
+- https://github.com/DEFRA/trade-imports-decision-comparer
+
+Still to configure:
+
+- https://github.com/DEFRA/btms-gateway
---
+++ compose/start-localstack.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed, installing..."
+    apt-get update
+    apt-get install -y jq
+else
+    echo "jq is already installed."
+fi
+
+export AWS_ENDPOINT_URL=http://localhost:4566
+export AWS_REGION=eu-west-2
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
+# processor
+aws --endpoint-url=http://localhost:4566 sqs create-queue \
+    --queue-name trade_imports_inbound_customs_declarations_processor.fifo \
+    --attributes FifoQueue=true
+
+# data api
+aws --endpoint-url=http://localhost:4566 sns create-topic \
+    --name trade_imports_data_upserted
+
+# decision-deriver
+aws --endpoint-url=http://localhost:4566 sqs create-queue \
+    --queue-name trade_imports_data_upserted_decision_deriver
+
+aws --endpoint-url=http://localhost:4566 sns subscribe \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    --protocol sqs \
+    --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_decision_deriver \
+    --attributes '{"RawMessageDelivery": "true"}'
+
+SUBSCRIPTION_ARN=$(aws --endpoint-url=http://localhost:4566 sns list-subscriptions-by-topic \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    | jq -r '.Subscriptions[] | select(.Endpoint == "arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_decision_deriver") | .SubscriptionArn')
+
+aws --endpoint-url=http://localhost:4566 sns set-subscription-attributes \
+    --subscription-arn $SUBSCRIPTION_ARN \
+    --attribute-name FilterPolicy \
+    --attribute-value '{"$or": [{"ResourceType": ["CustomsDeclaration"], "SubResourceType": ["ClearanceRequest"]}, {"ResourceType": ["ImportPreNotification"]}]}'
+
+# decision-comparer
+aws --endpoint-url=http://localhost:4566 sqs create-queue \
+    --queue-name trade_imports_data_upserted_decision_comparer
+
+aws --endpoint-url=http://localhost:4566 sns subscribe \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    --protocol sqs \
+    --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_decision_comparer \
+    --attributes '{"RawMessageDelivery": "true"}'
+
+SUBSCRIPTION_ARN=$(aws --endpoint-url=http://localhost:4566 sns list-subscriptions-by-topic \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    | jq -r '.Subscriptions[] | select(.Endpoint == "arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_decision_comparer") | .SubscriptionArn')
+
+aws --endpoint-url=http://localhost:4566 sns set-subscription-attributes \
+    --subscription-arn $SUBSCRIPTION_ARN \
+    --attribute-name FilterPolicy \
+    --attribute-value '{"ResourceType": ["CustomsDeclaration"], "SubResourceType": ["Finalisation"]}'
+
+function is_ready() {
+    # processor
+    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor.fifo || return 1
+    # data api
+    aws --endpoint-url=http://localhost:4566 sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_data_upserted')].TopicArn" || return 1
+    # decision-deriver
+    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver || return 1
+    # decision-comparer
+    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_data_upserted_decision_comparer || return 1
+    return 0
+}
+
+while ! is_ready; do
+    echo "Waiting until ready"
+    sleep 1
+done
+
+touch /tmp/ready
