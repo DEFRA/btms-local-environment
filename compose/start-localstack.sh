3e4b1febc4e9c1f49f78e8b98263c8f7693d65ed
Tim Peel <tim.peel@equalexperts.com>
2025-05-14 16:15:56 +0100

    Add gateway and associated config as entrypoint of system (#1)

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -14,14 +14,43 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
+# data api
+aws --endpoint-url=http://localhost:4566 sns create-topic \
+    --name trade_imports_data_upserted
+
+# gateway
+aws --endpoint-url=http://localhost:4566 sqs create-queue \
+    --queue-name trade_imports_data_upserted_btms_gateway
+
+aws --endpoint-url=http://localhost:4566 sns subscribe \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    --protocol sqs \
+    --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_btms_gateway \
+    --attributes '{"RawMessageDelivery": "true"}'
+
+SUBSCRIPTION_ARN=$(aws --endpoint-url=http://localhost:4566 sns list-subscriptions-by-topic \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_data_upserted \
+    | jq -r '.Subscriptions[] | select(.Endpoint == "arn:aws:sqs:eu-west-2:000000000000:trade_imports_data_upserted_btms_gateway") | .SubscriptionArn')
+
+aws --endpoint-url=http://localhost:4566 sns set-subscription-attributes \
+    --subscription-arn $SUBSCRIPTION_ARN \
+    --attribute-name FilterPolicy \
+    --attribute-value '{"$or": [{"ResourceType": ["CustomsDeclaration"], "SubResourceType": ["ClearanceDecision"]}, {"ResourceType": ["ProcessingError"]}]}'
+
+aws --endpoint-url=http://localhost:4566 sns create-topic \
+    --attributes FifoTopic=true \
+    --name trade_imports_inbound_customs_declarations.fifo
+
 # processor
 aws --endpoint-url=http://localhost:4566 sqs create-queue \
     --queue-name trade_imports_inbound_customs_declarations_processor.fifo \
     --attributes FifoQueue=true
 
-# data api
-aws --endpoint-url=http://localhost:4566 sns create-topic \
-    --name trade_imports_data_upserted
+aws --endpoint-url=http://localhost:4566 sns subscribe \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:trade_imports_inbound_customs_declarations.fifo \
+    --protocol sqs \
+    --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:trade_imports_inbound_customs_declarations_processor.fifo \
+    --attributes '{"RawMessageDelivery": "true"}'
 
 # decision-deriver
 aws --endpoint-url=http://localhost:4566 sqs create-queue \
@@ -62,10 +91,12 @@ aws --endpoint-url=http://localhost:4566 sns set-subscription-attributes \
     --attribute-value '{"ResourceType": ["CustomsDeclaration"], "SubResourceType": ["Finalisation"]}'
 
 function is_ready() {
-    # processor
-    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor.fifo || return 1
     # data api
     aws --endpoint-url=http://localhost:4566 sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_data_upserted')].TopicArn" || return 1
+    # gateway
+    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_data_upserted_btms_gateway || return 1
+    # processor
+    aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor.fifo || return 1
     # decision-deriver
     aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver || return 1
     # decision-comparer
