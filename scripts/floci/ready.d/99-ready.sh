658b1b17031d9c5cd14b34336ed551e9994623fd
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-08-25 19:38:36 +0100

    CMDS-1502: Resolve PR feedback

--- scripts/floci/ready.d/99-ready.sh
+++ scripts/floci/ready.d/99-ready.sh
@@ -1,42 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function is_ready() {
   # trade-imports-data-api
   awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_data_upserted')].TopicArn" || return 1
   awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_tracesched_upserted')].TopicArn" || return 1
-  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_queue-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_queue || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_queue-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_queue || return 1
 
   # btms-gateway
   awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_btms_activity')].TopicArn" || return 1
   awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_imports_inbound_customs_declarations.fifo')].TopicArn" || return 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_btms_gateway-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_btms_gateway || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_btms_gateway-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_btms_gateway || return 1
 
   # trade-imports-decision-deriver
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_decision_deriver || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_decision_deriver-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_tracesched_upserted_decision_deriver || return 1
 
   # trade-imports-processor
-  awslocal sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor-deadletter.fifo || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor.fifo || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_processor-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_processor || 1
-  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_updates_processor.fifo || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor-deadletter.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_inbound_customs_declarations_processor.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_processor-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_processor || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_updates_processor.fifo || return 1
 
   # trade-imports-reporting-api
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_reporting_api-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_reporting_api || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_btms_activity_reporting_api-deadletter || 1
-  awslocal sqs get-queue-url --queue-name trade_imports_btms_activity_reporting_api || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_reporting_api-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_data_upserted_reporting_api || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_btms_activity_reporting_api-deadletter || return 1
+  awslocal sqs get-queue-url --queue-name trade_imports_btms_activity_reporting_api || return 1
 
   # GMR Finder -> BTMS GVMS queue
-  awslocal sqs get-queue-url --queue-name trade_imports_matched_gmrs_btms_processor || 1
+  awslocal sqs get-queue-url --queue-name trade_imports_matched_gmrs_btms_processor || return 1
 
   return 0
 }
 
-while ! is_ready; do
-  echo "Waiting until ready..."
-  sleep 1
-done
+is_ready
